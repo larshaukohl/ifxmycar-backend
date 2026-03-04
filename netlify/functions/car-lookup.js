009375c5f7d614d76061ae8273f26651ff8849e2
larshaukohl <lars@larshaukohl.dk>
2026-03-04 21:21:39 +0100

    Update car-lookup.js

--- netlify/functions/car-lookup.js
+++ netlify/functions/car-lookup.js
@@ -1,1 +1,32 @@
+const https = require("https");
 
+exports.handler = async (event) => {
+  const plate = event.queryStringParameters?.plate;
+  
+  if (!plate) {
+    return { statusCode: 400, body: JSON.stringify({ error: "Mangler nummerplade" }) };
+  }
+
+  return new Promise((resolve) => {
+    const options = {
+      hostname: "api.motorapi.dk",
+      path: `/vehicles/${plate}`,
+      headers: { "X-Api-Key": process.env.MOTORAPI_KEY },
+      rejectUnauthorized: false
+    };
+
+    https.get(options, (res) => {
+      let data = "";
+      res.on("data", chunk => data += chunk);
+      res.on("end", () => resolve({
+        statusCode: res.statusCode,
+        headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+        body: data
+      }));
+    }).on("error", (err) => resolve({
+      statusCode: 500,
+      headers: { "Access-Control-Allow-Origin": "*" },
+      body: JSON.stringify({ error: err.message })
+    }));
+  });
+};
