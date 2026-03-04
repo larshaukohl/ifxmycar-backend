3de4664313c889a091cf17e2734168c543852428
larshaukohl <lars@larshaukohl.dk>
2026-03-04 21:40:34 +0100

    Update car-lookup.js

--- netlify/functions/car-lookup.js
+++ netlify/functions/car-lookup.js
@@ -1,42 +1,14 @@
-const https = require("https");
-const http = require("http");
-
 exports.handler = async (event) => {
   const plate = event.queryStringParameters?.plate;
+  const apiKey = process.env.MOTORAPI_KEY;
   
-  if (!plate) {
-    return { 
-      statusCode: 400, 
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify({ error: "Mangler nummerplade" }) 
-    };
-  }
-
-  const agent = new https.Agent({ rejectUnauthorized: false });
-
-  return new Promise((resolve) => {
-    const options = {
-      hostname: "api.motorapi.dk",
-      path: `/vehicles/${plate}`,
-      agent: agent,
-      headers: { 
-        "X-Api-Key": process.env.MOTORAPI_KEY,
-        "Accept": "application/json"
-      }
-    };
-
-    https.get(options, (res) => {
-      let data = "";
-      res.on("data", chunk => data += chunk);
-      res.on("end", () => resolve({
-        statusCode: res.statusCode,
-        headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
-        body: data
-      }));
-    }).on("error", (err) => resolve({
-      statusCode: 500,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify({ error: err.message })
-    }));
-  });
+  return {
+    statusCode: 200,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify({ 
+      plate: plate,
+      hasApiKey: !!apiKey,
+      apiKeyLength: apiKey ? apiKey.length : 0
+    })
+  };
 };
